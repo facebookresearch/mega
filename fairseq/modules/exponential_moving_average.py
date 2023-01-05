7d00ee418acfb55f1476ff1076e03d196540853f
violet-zct <chuntinz@meta.com>
2023-01-04 17:50:16 -0800

    bug in shift FFTs

--- fairseq/modules/exponential_moving_average.py
+++ fairseq/modules/exponential_moving_average.py
@@ -200,12 +200,12 @@ class MultiHeadEMA(nn.Module):
             fft_len = seq_len + kernel_size
             if self.bidirectional:
                 k1, k2 = torch.split(k, [self.embed_dim, self.embed_dim], dim=0)
+                k1, k2 = torch.split(k, [self.embed_dim, self.embed_dim], dim=0)
+                # D x K+L
                 if self.shift:
-                    k2 = k2[:, 1:]
-                    k = F.pad(k1, (0, seq_len)) + F.pad(k2.flip(-1), (seq_len + 1, 0))
-                else:
-                    # D x 2*L-1
                     k = F.pad(k1, (0, seq_len)) + F.pad(k2.flip(-1), (seq_len, 0))
+                else:
+                    k = F.pad(k1, (0, seq_len)) + F.pad(k2[:, :1], (0, fft_len - 1)) + F.pad(k2[:, 1:].flip(-1), (seq_len + 1, 0))
 
             k_f = torch.fft.rfft(k.float(), n=fft_len)
             x_f = torch.fft.rfft(x.float(), n=fft_len)
